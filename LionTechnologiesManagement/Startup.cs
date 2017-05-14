687ad2f2f82e869fc27e0efd8afb1f3f2f9a091d
user3546086 <sean86753@gmail.com>
2017-05-14 15:49:08 -0700

    Add project files.

---
+++ LionTechnologiesManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LionTechnologiesManagement.Startup))]
+namespace LionTechnologiesManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
